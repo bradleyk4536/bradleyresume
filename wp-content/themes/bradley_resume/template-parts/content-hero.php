d9a17b6d56c7b6aa53891286f3408bb842c72b78
Kenneth Bradley <k_bnet570@verizon.net>
2016-08-03 11:26:38 -0400

    Added dynamic project section

--- wp-content/themes/bradley_resume/template-parts/content-hero.php
+++ wp-content/themes/bradley_resume/template-parts/content-hero.php
@@ -1,8 +1,8 @@
 <?php $image_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>
 <?php if( has_post_thumbnail()) : ?>
-	<section class="sections-bg-alt parallax-layer no-margin-bottom padding-bottom-100 background" style="background: linear-gradient(rgba(78, 78, 78, 0.75), rgba(78, 78, 78, 0.75)), url( <?php echo $image_url; ?> ) no-repeat fixed; background-size: cover" data-stellar-background-ratio="0.5">
+	<section class="sections-bg-alt parallax-layer no-margin-bottom padding-bottom-100 background" style="background: url( <?php echo $image_url; ?> ) no-repeat fixed; background-size: cover" data-stellar-background-ratio="0.5">
 <?php else: ?>
-	<section class="sections-bg-alt parallax-layer parallax-header no-margin-bottom padding-bottom-100 background" data-stellar-background-ratio="0.5">
+	<section class="sections-bg-alt parallax-layer parallax-header no-margin-bottom padding-bottom-100 background what" data-stellar-background-ratio="0.5">
 <?php endif; ?>
 	<div class="container">
 		<div class="row">
@@ -39,7 +39,7 @@
 				<h1 class="page-section text-center margin-top-100" style="color: rgba(250, 250, 250, .8); margin-top: 0px;">
 					<strong><?php echo get_field( 'first_name'); echo " "; echo get_field( 'last_name' ); ?> </strong>
 				</h1>
-				<h2 class="tagline"><?php echo get_post_meta(8, 'Tag Line 1', true); ?></h2>
+				<h2 class="tagline"><?php bloginfo('description'); ?></h2>
 				<h2 class="tagline"><?php echo get_post_meta(8, 'Tag Line 2', true); ?></h2>
 				<a class="btn btn-lg btn-primary margin-top-30" href="#">HIRE ME</a>
 			</div>
--- wp-content/themes/bradley_resume/template-parts/content-projects.php
+++ wp-content/themes/bradley_resume/template-parts/content-projects.php
@@ -1,116 +1,102 @@
 <section class="section_header">
 	<div class="container">
-		<div class="row">
+		<div class="row divider">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<hr />
-				<h3 class="page-section text-center">
-					<strong>PROJECTS</strong>
-				</h3>
-				<p class="lead text-center divider">
-					Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
-				</p>
+				<h3 class="page-section text-center profile"><?php echo get_field('project_header'); ?></h3>
+
+			<?php if(!empty(get_field('project_statement'))) : ?>
+				<p class="lead text-center"><?php the_field('project_statement'); ?></p>
+			<?php endif; ?>
+
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="min-height: 1px;">
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div id="carousel-347740" class="carousel slide custom-controls-slider">
 					<div class="carousel-inner">
 						<div class="item active">
 							<div class="row">
+			<?php $counter = 0; ?>
+			<?php $loop = new Wp_Query(array('post_type' => 'projects', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+							<?php while($loop->have_posts() && $counter < 4) : $loop->the_post(); $counter++; ?>
 								<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 text-center">
 									<div class="thumbnail relative">
+									<?php if(has_post_thumbnail()) : ?>
+<!--			GIVE A FEATURE IMAGE A CLASS						-->
+										<?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive img-center')); ?>
+									<?php else : ?>
 										<img class="img-responsive img-center" src="<?php bloginfo('template_directory'); ?>/assets/images/img-member-thumb.jpg" alt="thumb" />
+									<?php endif; ?>
+
 										<div class="box-wrap">
-											<h4>
-												Omnifoods<br /><small>CEO, marketing guru</small>
+											<h4><?php the_title(); ?>
+											<?php if(!empty(get_field( 'additional_description' ))) : ?>
+												<br /><small><?php the_field( 'additional_description' ); ?></small>
+											<?php endif; ?>
 											</h4>
 											<ul class="list-inline">
+											<?php if(!empty(get_field( 'github_url' ))) : ?>
 												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-github-alt"></i></a>
+													<a class="circle-icon-bg" href="<?php the_field( 'github_url' ); ?>"><i class="fa fa-github-alt"></i></a>
 												</li>
+											<?php endif; ?>
 												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-book"></i></a>
+													<a class="circle-icon-bg" href="<?php the_permalink(); ?>"><i class="fa fa-book"></i></a>
 												</li>
 											</ul>
-											<p>Hand coded Object Oriented PHP CMS</p>
+											<p><?php the_excerpt(); ?></p>
 										</div>
 									</div>
 								</div>
-								<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 text-center">
-									<div class="thumbnail relative">
-										<img class="img-responsive img-center" src="<?php bloginfo('template_directory'); ?>/assets/images/img-member-thumb.jpg" alt="thumb" />
-										<div class="box-wrap">
-											<h4>
-												JOHN DOE<br /><small>CEO, marketing guru</small>
-											</h4>
-											<ul class="list-inline">
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-dribbble fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-facebook fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-twitter fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-google-plus fa-2x"></i></a>
-												</li>
-											</ul>
-										</div>
-									</div>
-								</div>
-								<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 text-center">
-									<div class="thumbnail relative">
-										<img class="img-responsive img-center" src="<?php bloginfo('template_directory'); ?>/assets/images/img-member-thumb.jpg" alt="thumb" />
-										<div class="box-wrap">
-											<h4>
-												JOHN DOE<br /><small>CEO, marketing guru</small>
-											</h4>
-											<ul class="list-inline">
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-dribbble fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-facebook fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-twitter fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-google-plus fa-2x"></i></a>
-												</li>
-											</ul>
-										</div>
-									</div>
-								</div>
-								<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 text-center">
-									<div class="thumbnail relative">
-										<img class="img-responsive img-center" src="<?php bloginfo('template_directory'); ?>/assets/images/img-member-thumb.jpg" alt="thumb" />
-										<div class="box-wrap">
-											<h4>
-												JOHN DOE<br /><small>CEO, marketing guru</small>
-											</h4>
-											<ul class="list-inline">
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-dribbble fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-facebook fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-twitter fa-2x"></i></a>
-												</li>
-												<li>
-													<a class="circle-icon-bg" href="#"><i class="fa fa-google-plus fa-2x"></i></a>
-												</li>
-											</ul>
-										</div>
-									</div>
-								</div>
+							<?php endwhile; ?>
 							</div>
 
 						</div>
+		<!--		ANOTHER LOOP FOR MORE PROJECTS NORMAL DISPLAY IS FOUR ACROSS			-->
+				<?php if($counter === 4 && $counter < 8) : ?>
+					<div class="item">
+						<div class="row">
+							<?php while($loop->have_posts()) : $loop->the_post(); $counter++; ?>
+								<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 text-center">
+									<div class="thumbnail relative">
+									<?php if(has_post_thumbnail()) : ?>
+<!--			GIVE A FEATURE IMAGE A CLASS						-->
+										<?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive img-center')); ?>
+									<?php else : ?>
+										<img class="img-responsive img-center" src="<?php bloginfo('template_directory'); ?>/assets/images/img-member-thumb.jpg" alt="thumb" />
+									<?php endif; ?>
+										<div class="box-wrap">
+											<h4><?php the_title(); ?>
+											<?php if(!empty(get_field( 'additional_description' ))) : ?>
+												<br /><small><?php the_field( 'additional_description' ); ?></small>
+											<?php endif; ?>
+											</h4>
+											<ul class="list-inline">
+											<?php if(!empty(get_field( 'github_url' ))) : ?>
+												<li>
+													<a class="circle-icon-bg" href="<?php the_field( 'github_url' ); ?>"><i class="fa fa-github-alt"></i></a>
+												</li>
+											<?php endif; ?>
+
+												<li>
+													<a class="circle-icon-bg" href=""><i class="fa fa-book"></i></a>
+												</li>
+
+											</ul>
+											<p><?php the_excerpt(); ?></p>
+										</div>
+									</div>
+								</div>
+							<?php endwhile; wp_reset_postdata(); ?>
+						</div>
+						</div>
+					<?php endif; ?>
 					</div>
+<!--		SHOW NAVIGATION IF PROJECTS ARE GREATER THAN 4			-->
+		<?php if($counter > 4) :  ?>
 					 <a class="left carousel-control" href="#carousel-347740" data-slide="prev"><i class="fa fa-angle-left fa-4x"></i></a> <a class="right carousel-control" href="#carousel-347740" data-slide="next"><i class="fa fa-angle-right fa-4x"></i></a>
+		<?php endif; ?>
 				</div>
 			</div>
 		</div>
