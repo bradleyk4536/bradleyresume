211c88520cdc830174243c8e469bff92a4f5d06d
Kenneth Bradley <k_bnet570@verizon.net>
2016-08-10 11:25:47 -0400

    Finish styling resume section

--- wp-content/themes/bradley_resume/template-parts/content-resume_middle_section.php
+++ wp-content/themes/bradley_resume/template-parts/content-resume_middle_section.php
@@ -8,8 +8,34 @@
 			<i class="<?php the_field('middle_icon'); ?>" aria-hidden="true"></i>
 		<?php endif; ?>
 
-			  <?php echo get_field('middle_section_header'); ?></h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error dolorum doloremque doloribus ut itaque, vitae assumenda corporis veniam quo in dicta voluptatem ipsam, pariatur consequatur, aperiam. Delectus praesentium vero Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi officia, ex excepturi ducimus illo harum aliquid minus architecto eveniet! Non, ea animi! Amet beatae laboriosam maxime eveniet quos Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error dolorum doloremque doloribus ut itaque, vitae assumenda corporis veniam quo in dicta voluptatem ipsam, pariatur consequatur, aperiam. Delectus praesentium vero Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi officia, ex excepturi ducimus illo harum aliquid minus architecto eveniet! Non, ea animi! Amet beatae laboriosam maxime eveniet quos </p>
+		<?php echo get_field('middle_section_header'); ?></h4>
+
+		<?php $loop = new Wp_Query(array('post_type' => 'military', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+		<?php while($loop->have_posts()) : $loop->the_post();
+
+				$militaryEnd = null;
+				$militaryStart = new DateTime(get_field('service_start_date'));
+
+				if(!empty(get_field('service_end_date'))) :
+					$militaryEnd = new DateTime(get_field('service_end_date'));
+				endif;
+		?>
+		<div class="military">
+		<h5>
+
+		<span class="company"><?php the_title(); ?></span>
+
+		<h6><?php the_field('discharge_type'); ?></h6>
+
+		<span class="startDate"><?php echo $militaryStart->format('m/Y'); ?></span>
+		&mdash;
+		<span class="endDate"><?php if(!(empty($militaryEnd))) : echo $militaryEnd->format('m/Y'); else: echo 'Present'; endif; ?></span>
+		</h5>
+
+		<p><?php the_field('job_description'); ?></p>
+		</div>
+			<hr>
+		<?php endwhile; wp_reset_postdata(); ?>
 			</div>
 		</div>
 	</article>
--- wp-content/themes/bradley_resume/template-parts/content-resume_top_section.php
+++ wp-content/themes/bradley_resume/template-parts/content-resume_top_section.php
@@ -7,8 +7,38 @@
 			<i class="<?php the_field('top_icon'); ?>" aria-hidden="true"></i>
 		<?php endif; ?>
 
-			<?php echo get_field('top_section_header'); ?></h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error dolorum doloremque doloribus ut itaque, vitae assumenda corporis veniam quo in dicta voluptatem ipsam, pariatur consequatur, aperiam. Delectus praesentium vero Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi officia, ex excepturi ducimus illo harum aliquid minus architecto eveniet! Non, ea animi! Amet beatae laboriosam maxime eveniet quos Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error dolorum doloremque doloribus ut itaque, vitae assumenda corporis veniam quo in dicta voluptatem ipsam, pariatur consequatur, aperiam. Delectus praesentium vero Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi officia, ex excepturi ducimus illo harum aliquid minus architecto eveniet! Non, ea animi! Amet beatae laboriosam maxime eveniet quosLorem ipsum dolor sit amet, consectetur adipisicing elit. Error dolorum doloremque doloribus ut itaque, vitae assumenda corporis veniam quo in dicta voluptatem ipsam, pariatur consequatur, aperiam. Delectus praesentium vero Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos sequi officia, ex excepturi ducimus illo harum aliquid minus architecto eveniet! Non, ea animi! Amet beatae laboriosam maxime eveniet quos </p>
+		<?php echo get_field('top_section_header'); ?></h4>
+		<?php $loop = new Wp_query(array('post_type' => 'work_experience', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+			<?php while($loop->have_posts()) : $loop->the_post();
+
+				$workEnd = null;
+				$workStart = new DateTime(get_field('start_date'));
+
+				if(!empty(get_field('end_date'))) :
+					$workEnd = new DateTime(get_field('end_date'));
+				endif;
+			?>
+		<div class="work">
+		<h5>
+
+		<span class="company"><?php the_title(); ?></span>
+
+
+		<span class="city"><?php the_field('company_city'); ?></span>,
+
+		<span class="state"><?php the_field('company_state'); ?></span>
+
+		</h5>
+		<h6><?php the_field('job_title'); ?></h6>
+
+		<span class="startDate"><?php echo $workStart->format('m/Y'); ?> &mdash; </span>
+
+		<span class="endDate"><?php if(!(empty($workEnd))) : echo $workEnd->format('m/Y'); else: echo 'Present'; endif; ?></span>
+
+			 <?php the_field('description'); ?>
+		</div>
+
+		<?php endwhile; wp_reset_postdata(); ?>
 			</div>
 		</div>
 	</article>
